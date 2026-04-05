5d1455dac28c401b1b1d1aefde654e2c20c31347
TuanNguyen1611 <hungtuan.161106@gmail.com>
2026-04-05 17:01:23 +0700

    Complete section 16

--- Lab02/src/Aims.java
+++ Lab02/src/Aims.java
@@ -1,19 +1,12 @@
 public class Aims {
     public static void main(String[] args) {
-        Cart anOrder = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", "Unknown", 90, 18.99f);
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc(
-                "The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc(
-                "Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc(
-                "Aladin", "Animation", "Unknown", 90, 18.99f);
-
-        anOrder.addDigitalVideoDisc(dvd1, dvd2);
-
-        DigitalVideoDisc[] dvdList = {dvd3};
-        anOrder.addDigitalVideoDisc(dvdList);
-
-        anOrder.print();
+        System.out.println("DVD 1 ID: " + dvd1.getId());
+        System.out.println("DVD 2 ID: " + dvd2.getId());
+        System.out.println("DVD 3 ID: " + dvd3.getId());
+        System.out.println("Total DVDs created: " + DigitalVideoDisc.getNbDigitalVideoDiscs());
     }
 }
--- Lab02/src/DigitalVideoDisc.java
+++ Lab02/src/DigitalVideoDisc.java
@@ -1,21 +1,32 @@
 public class DigitalVideoDisc {
+    private static int nbDigitalVideoDiscs = 0;
+
+    private int id;
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
 
+    private void assignId() {
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+    }
+
     public DigitalVideoDisc(String title) {
+        assignId();
         this.title = title;
     }
 
     public DigitalVideoDisc(String title, String category, float cost) {
+        assignId();
         this.title = title;
         this.category = category;
         this.cost = cost;
     }
 
     public DigitalVideoDisc(String title, String category, String director, float cost) {
+        assignId();
         this.title = title;
         this.category = category;
         this.director = director;
@@ -23,11 +34,20 @@ public class DigitalVideoDisc {
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        assignId();
         this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
     }
 
     public String getTitle() {
